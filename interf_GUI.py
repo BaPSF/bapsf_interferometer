d1484ff1638face53a111ee9b203fad6246c99c5
Jia <acoruscalamuslinn@gmail.com>
2024-07-13 18:28:08 -0700

    plotting GUI ver1.0 finish; tested with exisiting data

--- interf_GUI.py
+++ interf_GUI.py
@@ -1,3 +1,13 @@
+# coding: utf-8
+
+'''
+This module contains functions for plotting interferometer data in real-time.
+The plotted data reads hdf5 files saved using interf_main.py
+
+Author: Jia Han
+Ver1.0 created on: 2021-07-13
+'''
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QPushButton, QWidget
 from PyQt5.QtCore import QThread, pyqtSignal, QObject
 
@@ -9,13 +19,14 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 import numpy as np
 import h5py
 import os
+import time
 
 from interf_raw import get_calibration_factor
 
 #===============================================================================================================================================
 #===============================================================================================================================================
 
-def get_latest_file(dir_path):
+def get_latest_file(dir_path=r"C:\data\LAPD\interferometer_samples"):
     """
     This function returns the latest file in a directory.
 
@@ -30,105 +41,129 @@ def get_latest_file(dir_path):
     return max(full_path_file_list, key=os.path.getctime)
 
 def get_data(ifn, i):
+    '''
+    read the data from the hdf5 file
+    '''
     with h5py.File(ifn, 'r', swmr=True) as f:
         dataset_names = list(f['phase_p20'].keys())
-        last_dataset_name = dataset_names[i]
-        phaseA = np.array(f['phase_p20'][last_dataset_name])
-        phaseB = np.array(f['phase_p29'][last_dataset_name])
-        t_ms = np.array(f['time_array'][last_dataset_name])
-    return t_ms, phaseA, phaseB
+        dataset_name = dataset_names[i]
+        phaseA = np.array(f['phase_p20'][dataset_name])
+        phaseB = np.array(f['phase_p29'][dataset_name])
+        t_ms = np.array(f['time_array'][dataset_name])
+
+    return t_ms, phaseA, phaseB, time.ctime(float(dataset_name))
 
 #===============================================================================================================================================
 #===============================================================================================================================================
 
 class Worker(QObject):
-    data_updated = pyqtSignal(np.ndarray, np.ndarray, np.ndarray)  # Signal to emit the data
+    data_updated = pyqtSignal(np.ndarray, np.ndarray, np.ndarray, str)  # Signal to emit the data
 
-    def __init__(self, ifn):
+    def __init__(self):
         super().__init__()
-        self.dir_path = r"C:\data\LAPD\interferometer_samples"
         self.i = 0
 
     def run(self):
-        # This method will run in a separate thread
-        # Replace this with your actual data update logic
+        '''
+        Worker function that emits the data to the main thread; this method will run in a separate thread
+        Find the latest file and read the last indexed data from it
+        '''
         while True:
-            ifn = get_latest_file(self.dir_path)
-            t_ms, phaseA, phaseB = get_data(ifn, self.i)
+            ifn = get_latest_file()
+            t_ms, phaseA, phaseB, tstamp = get_data(ifn, -1)
             neA = phaseA * get_calibration_factor(288e9)
             neB = phaseB * get_calibration_factor(282e9)
-            self.data_updated.emit(t_ms, neA, neB)
+            self.data_updated.emit(t_ms, neA, neB, tstamp)
             self.i += 1
             QThread.sleep(1)  # Simulate some delay
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        super().__init__() # Call the parent class constructor
+        central_widget = QWidget() # Create a central widget
+        self.setCentralWidget(central_widget) # Set the central widget
 
+        # Create a layout for the central widget
         layout = QVBoxLayout(central_widget)
         layout.addWidget(QLabel("Interferometer Plot"))
+        # Create a button to start the plot
         button = QPushButton("Start Plot")
         layout.addWidget(button)
         button.clicked.connect(self.start_plot)
+        # Create a button to save the current trace
+        self.keep_trace_button = QPushButton("Save current trace on plot")
+        layout.addWidget(self.keep_trace_button)
+        self.keep_trace_button.clicked.connect(self.keep_trace)
+        # Create a button to remove the saved trace from keep_trace
+        self.remove_trace_button = QPushButton("Remove saved trace from plot")
+        layout.addWidget(self.remove_trace_button)
+        self.remove_trace_button.clicked.connect(self.remove_trace)
 
-        self.autoscale_enabled = True  # Flag to enable/disable autoscale
-        toggle_button = QPushButton("Toggle Autoscale")
-        layout.addWidget(toggle_button)
-        toggle_button.clicked.connect(self.toggle_autoscale)
-
+        # Create a figure and a canvas for the figure
         self.fig = Figure()
         self.ax = self.fig.add_subplot(111)
         self.canvas = FigureCanvas(self.fig)  # Create a canvas for the figure
         layout.addWidget(self.canvas)  # Add the canvas to the layout
-
-        # Add the navigation toolbar
+        # Add the navigation toolbar for interacting with plot
         self.toolbar = NavigationToolbar(self.canvas, self)
         layout.addWidget(self.toolbar)
-
+        # Plot label and title
         self.ax.set_xlabel('Time (ms)')
         self.ax.set_ylabel('ne (m^-3)')
         self.ax.set_title('Density assuming 40cm plasma Dia')
-        self.ax.legend()
-
+        # Create the plot lines
         self.line_neA, = self.ax.plot([], [], 'r-')
         self.line_neB, = self.ax.plot([], [], 'g-')
 
-        self.ifn = r"C:\data\LAPD\interferometer_samples"
+        # Updating the plot by reading data from hdf5; use thread to avoid blocking the GUI
+        
         self.thread = QThread()  # Thread for running the worker
-        self.worker = Worker(self.ifn)  # Worker object
+        self.worker = Worker()  # Worker object
         self.worker.moveToThread(self.thread)  # Move worker to the thread
         self.worker.data_updated.connect(self.update_plot)  # Connect signal
         self.thread.started.connect(self.worker.run)  # Start worker.run when the thread starts
 
         self.update_count = 0  # Counter for testing
 
-    def toggle_autoscale(self):
-        # Toggle the autoscale state
-        self.autoscale_enabled = not self.autoscale_enabled
-
-
     def start_plot(self):
         self.thread.start()  # Start the thread, which starts worker.run
 
-    def update_plot(self, x, y_neA, y_neB):
+    def update_plot(self, x, y_neA, y_neB, label):
         # Update the plot with new data
         self.line_neA.set_data(x, y_neA)
         self.line_neB.set_data(x, y_neB)
+        self.line_neA.set_label('P20  ' + label)
+        self.line_neB.set_label('P29  ' + label)
+        self.ax.legend()
         self.ax.relim()
-
-        if self.autoscale_enabled:
-            self.ax.autoscale_view(True, True, True)
-        else:
-            # If autoscale is disabled, you might want to handle it differently
-            pass
+        self.ax.autoscale_view(True, True, True)
 
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
         self.update_count += 1  # Increment the update counter
         print(f"Plot updated: {self.update_count}")  # Print the update count
+
+    def keep_trace(self):
+        # Save the current trace
+        x = self.line_neA.get_xdata()
+        y_neA = self.line_neA.get_ydata()
+        y_neB = self.line_neB.get_ydata()
+        label = self.line_neA.get_label()
+        self.ax.plot(x, y_neA, 'r--', alpha=0.5, label=label)
+        self.ax.plot(x, y_neB, 'g--', alpha=0.5, label=label)
+        self.ax.legend()
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
+    def remove_trace(self):
+        # Remove the saved trace from the plot
+        for line in self.ax.lines:
+            if line.get_linestyle() == '--':
+                line.remove()
+        self.ax.legend()
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
 
 
 if __name__ == "__main__":
